ea81eba57e3f725d96ebfe2f8661ff250b50b100
Justin Tomlinson <justintomlinsonn@gmail.com>
2018-02-17 00:45:04 -0500

    Added square area calculator
    Currently buggy when not inputting numbers.

--- app/src/main/java/jandj/buildingbuilder/MainActivity.java
+++ app/src/main/java/jandj/buildingbuilder/MainActivity.java
@@ -2,6 +2,12 @@ package jandj.buildingbuilder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView hw = findViewById(R.id.text);
+        final EditText et1 = findViewById(R.id.et1), et2 = findViewById(R.id.et2);
+        final Button b = findViewById(R.id.button);
+
+        et1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().equals(""))
+                    b.setText(String.valueOf(Float.parseFloat(charSequence.toString()) *
+                        Float.parseFloat(et2.getText().toString())));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        et2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().equals(""))
+                    b.setText(String.valueOf(Float.parseFloat(et1.getText().toString()) *
+                        Float.parseFloat(charSequence.toString())));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b.setText(String.valueOf(Float.parseFloat(et1.getText().toString()) *
+                        Float.parseFloat(et2.getText().toString())));
+            }
+        });
     }
 }
